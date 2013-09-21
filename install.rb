23afc10ed001397d3de0fb6d7a5c79623f89518e
Myles Megyesi <mylesmegyesi@me.com>
2013-09-20 21:23:59 -0500

    adds support for copying directories

--- install.rb
+++ install.rb
@@ -9,15 +9,25 @@ TARGET_OWNER = ARGV[1]
 def cp(local, target)
   full_local  = File.join(LOCAL_ROOT, local)
   full_target = File.join(TARGET_ROOT, target)
-  FileUtils.rm full_target if File.exist?(full_target)
-  full_target_dir = File.dirname(full_target)
-  FileUtils.mkpath(full_target_dir)
-  FileUtils.chown TARGET_OWNER, TARGET_OWNER, full_target_dir
-  FileUtils.cp full_local, full_target
-  FileUtils.chown TARGET_OWNER, TARGET_OWNER, full_target
+  if File.directory?(full_local)
+    FileUtils.rm_rf full_target if File.exist?(full_target)
+    full_target_dir = File.dirname(full_target)
+    FileUtils.mkpath(full_target_dir)
+    FileUtils.chown TARGET_OWNER, TARGET_OWNER, full_target_dir
+    FileUtils.cp_r File.join(full_local, '.'), full_target
+    FileUtils.chown TARGET_OWNER, TARGET_OWNER, full_target
+  else
+    FileUtils.rm full_target if File.exist?(full_target)
+    full_target_dir = File.dirname(full_target)
+    FileUtils.mkpath(full_target_dir)
+    FileUtils.chown TARGET_OWNER, TARGET_OWNER, full_target_dir
+    FileUtils.cp full_local, full_target
+    FileUtils.chown TARGET_OWNER, TARGET_OWNER, full_target
+  end
 end
 
 cp('zshrc', '.zshrc')
 cp('zsh-syntax-highlighting.zsh', '.zsh/zsh-syntax-highlighting.zsh')
+cp('zsh-syntax-highlighting', '.zsh/zsh-syntax-highlighting')
 cp('prompt.zsh', '.zsh/prompt.zsh')
 cp('vcprompt', '.bin/vcprompt')
