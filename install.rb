2485e067a3f35fea76b5c69d20426b9e6c175dff
Myles Megyesi <mylesmegyesi@me.com>
2013-09-24 17:15:03 -0500

    remove debug fail

--- install.rb
+++ install.rb
@@ -65,6 +65,5 @@ cp 'rspec',                       '.rspec'
 
 # Make ZSH the default login shell
 zsh_path = `which zsh`.split(/ /).first
-fail "aaaaaaaaaaaaaaaaaaaaaaaaaa #{zsh_path} aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: #{zsh_path}"
 sh "chsh -s #{zsh_path} \"#{TARGET_OWNER}\""
 
