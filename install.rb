4d4bd5c1e0f23a156c9ceaefdbb204da78a8c9fb
Myles Megyesi <mylesmegyesi@me.com>
2013-09-24 17:11:42 -0500

    i can't figure this out

--- install.rb
+++ install.rb
@@ -65,6 +65,6 @@ cp 'rspec',                       '.rspec'
 
 # Make ZSH the default login shell
 zsh_path = IO.popen('which zsh').readlines.first
-fail "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: #{zsh_path}"
+fail "aaaaaaaaaaaaaaaaaaaaaaaaaa #{zsh_path} aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: #{zsh_path}"
 sh "chsh -s #{zsh_path} \"#{TARGET_OWNER}\""
 
