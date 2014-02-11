3f4f86565c6d0b420a01f4ffde83e451e49cf312
Myles Megyesi <mylesmegyesi@me.com>
2014-02-11 13:54:35 -0600

    removes ruby env variables

--- ruby.zsh
+++ ruby.zsh
@@ -9,9 +9,3 @@ alias bxr="bundle exec rspec"
 alias bxc="bundle exec rake cucumber"
 alias cuke="bundle exec cucumber"
 alias rage="be rake"
-
-export RUBY_HEAP_MIN_SLOTS=1000000
-export RUBY_HEAP_SLOTS_INCREMENT=1000000
-export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
-export RUBY_GC_MALLOC_LIMIT=1000000000
-export RUBY_HEAP_FREE_MIN=500000
